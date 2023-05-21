2efe7e25f61b8598693653b4843a9b456cda312a
Qamar Ali <iamqamarali1@gmail.com>
2023-05-22 02:45:17 +0500

    testing deployment

--- server/api/posts/index.js
+++ server/api/posts/index.js
@@ -5,8 +5,9 @@ export default defineEventHandler(async (event) => {
     page = parseInt(page) || 1
     page--
     if(!limit){
-        limit = 10
+        limit = parseInt(limit) || 10
     }
+    
     const posts = await Post.latest(page * limit, limit)
     return  posts.map(post => post.except([]))
 })
