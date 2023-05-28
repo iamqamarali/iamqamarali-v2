41a114779d5fb74da79118baabec113c970c3941
Qamar Ali <iamqamarali1@gmail.com>
2023-05-28 13:27:05 +0500

    fiexed .env.exmaple

--- server/api/backblaze/deleteFile.post.js
+++ server/api/backblaze/deleteFile.post.js
@@ -8,8 +8,8 @@ export default defineEventHandler(async (event)=>{
 
     // create a new B2 instance
     const b2 = new B2({
-        applicationKeyId: process.env.BLACK_BLAZE_KEY_ID,
-        applicationKey: process.env.BLACK_BLAZE_APPLICATION_KEY
+        applicationKeyId: process.env.BACK_BLAZE_KEY_ID,
+        applicationKey: process.env.BACK_BLAZE_APPLICATION_KEY
     });
 
     // authorize with B2
--- server/api/backblaze/getUploadUrl.post.js
+++ server/api/backblaze/getUploadUrl.post.js
@@ -11,8 +11,8 @@ export default defineEventHandler(async (event)=>{
 
     // create a new B2 instance
     const b2 = new B2({
-        applicationKeyId: process.env.BLACK_BLAZE_KEY_ID,
-        applicationKey: process.env.BLACK_BLAZE_APPLICATION_KEY
+        applicationKeyId: process.env.BACK_BLAZE_KEY_ID,
+        applicationKey: process.env.BACK_BLAZE_APPLICATION_KEY
     });
 
     // authorize with B2
@@ -32,7 +32,7 @@ export default defineEventHandler(async (event)=>{
         authorizationToken: res.data.authorizationToken,
         uploadUrl: res.data.uploadUrl, 
         fileName: fileName,
-        fileUrl: process.env.BLACK_BLAZE_BASE_URL + '/' + fileName,
+        fileUrl: process.env.BACK_BLAZE_BASE_URL + '/' + fileName,
     };
 
 })
