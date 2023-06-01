a40d109f0acf6f2cb89c966a35597f98856e1fc9
Qamar Ali <iamqamarali1@gmail.com>
2023-06-02 03:23:29 +0500

    asd

--- middleware/addAnimation.global.js
+++ middleware/addAnimation.global.js
@@ -1,4 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    
+    const getDepth = (path) => {
+        return path.split('/').filter(seg => seg.length> 0 ).length
+    }
+
+    const toDepth = getDepth(to.path)
+    const fromDepth = getDepth(from.path)
+
+    // if (toDepth > fromDepth) {
+    //     to.meta.transitionName = 'slide-left'
+    //     from.meta.pageTransition = 'slide-right'
+    // } else if (toDepth < fromDepth) {
+    //     to.meta.transitionName = 'slide-right'
+    //     from.meta.pageTransition = 'slide-left'
+    // }
 
 })
