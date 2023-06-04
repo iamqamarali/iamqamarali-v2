5a0b12f2c7d3f9b1a32b80b4c286b9b363ccede3
Qamar Ali <iamqamarali1@gmail.com>
2023-06-05 00:10:56 +0500

    asd

--- server/plugins/Init-database.js
+++ server/plugins/Init-database.js
@@ -4,4 +4,5 @@ import mysql from 'mysql2/promise';
 export default defineNitroPlugin(async () => {
     const db = await mysql.createConnection(process.env.DATABASE_URL);
     BaseModel.setConnection(db);
+    return {  nessage : "connected" }
 })
